e4dd5edfcb86b9f08babd8f3c0ea041df795ac85
kendrick whitman <mr_roboto11@protonmail.com>
2023-10-17 04:04:55 -0600

    update

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const Datastore = require('nedb');
 require('dotenv').config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Loading host ${port}`);
 });
