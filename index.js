3e6fb8a6ed500af1e50f28647a6b02120a8aff45
kendrick whitman <mr_roboto11@protonmail.com>
2023-10-17 04:21:43 -0600

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.listen(port, () => {
   console.log(`Loading host ${port}`);
 });
 app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
+app.use(express.json({ limit: '200mb' }));
 
 const database = new Datastore('weather-history.db');
 database.loadDatabase();
